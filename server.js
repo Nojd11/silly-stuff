c623164b1163a9befa3e76e8616187c7847db873
Nojd11 <104686952+Nojd11@users.noreply.github.com>
2024-05-03 19:58:42 -0400

    working?

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const http = require('http')
+const PORT = process.env.PORT || 8080
+const socketio = require('socket.io')
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")))
+
+// Start server
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+// Handle a socket connection request from web client
+const connections = [null, null]
+
